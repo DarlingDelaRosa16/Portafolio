9f2853d9bc2eb5ba8bb0605f20d9ee4f05337998
Darling <dnomar165@gmail.com>
2021-09-17 01:10:11 -0400

    Adding configurations to the form for send email to Netlify

--- src/Components/Contact/contact.jsx
+++ src/Components/Contact/contact.jsx
@@ -12,14 +12,18 @@ const Contact = () => {
             <ContentBox id="contact">
                     <ContactContent >
                         <ContactTitle >Contact me</ContactTitle>
-                        <form id="contactInformation">
-                            <ContactLabel gridColumn={"1/2"} >Name</ContactLabel>
-                            <ContactTextBox gridColumn={"1/2"}  />
-                            <ContactLabel gridColumn={"1/2"} >Email</ContactLabel>
-                            <ContactTextBox gridColumn={"1/2"} />
-                            <ContactLabel gridColumn={"2/3"} gridRow={"1/2"} gridC={"1/2"} gridR={"auto"} >Message</ContactLabel>
-                            <ContactTextArea id="contactArea"/>
-                            <ContactButton>Send</ContactButton>                        
+                        <form name="contact" id="contactInformation" method="POST" data-netlify="true">
+                            
+                            <ContactLabel htmlFor="name" gridColumn={"1/2"} >Name</ContactLabel>
+                            <ContactTextBox name="name" gridColumn={"1/2"}  />
+                            
+                            <ContactLabel htmlFor="email" gridColumn={"1/2"} >Email</ContactLabel>
+                            <ContactTextBox name="email" gridColumn={"1/2"} />
+
+                            <ContactLabel htmlFor="message" gridColumn={"2/3"} gridRow={"1/2"} gridC={"1/2"} gridR={"auto"} >Message</ContactLabel>
+                            <ContactTextArea name="message" id="contactArea"/>
+
+                            <ContactButton type="submit" >Send</ContactButton>                        
                         </form>
                     </ContactContent>
                 </ContentBox>
@@ -29,14 +33,18 @@ const Contact = () => {
             <ContentBox id="contact" >
                     <ContactContent >
                         <ContactTitle >Contactame</ContactTitle>
-                        <form name="Contact" id="contactInformation" netlify>
-                            <ContactLabel gridColumn={"1/2"} >Nombre</ContactLabel>
-                            <ContactTextBox gridColumn={"1/2"}  />
-                            <ContactLabel gridColumn={"1/2"} >Email</ContactLabel>
-                            <ContactTextBox gridColumn={"1/2"} />
-                            <ContactLabel gridColumn={"2/3"} gridRow={"1/2"} gridC={"1/2"} gridR={"auto"} >Mensaje</ContactLabel>
-                            <ContactTextArea id="contactArea"/>
-                            <ContactButton>Enviar</ContactButton>                        
+                        <form name="contact" id="contactInformation" method="POST" data-netlify="true">
+                            
+                            <ContactLabel htmlFor="name" gridColumn={"1/2"} >Nombre</ContactLabel>
+                            <ContactTextBox name="name" type="text" gridColumn={"1/2"}  />
+
+                            <ContactLabel htmlFor="email" gridColumn={"1/2"} >Email</ContactLabel>
+                            <ContactTextBox name="email" type="email" gridColumn={"1/2"} />
+
+                            <ContactLabel htmlFor="message" gridColumn={"2/3"} gridRow={"1/2"} gridC={"1/2"} gridR={"auto"} >Mensaje</ContactLabel>
+                            <ContactTextArea name="message" id="contactArea"/>
+
+                            <ContactButton type="submit">Enviar</ContactButton>                        
                         </form>
                     </ContactContent>
                 </ContentBox>
