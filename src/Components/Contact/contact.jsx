33fe620083512b346f66316ca552cf6230355db3
Darling <dnomar165@gmail.com>
2021-09-19 23:10:32 -0400

    doing a test in (Contact)

--- src/Components/Contact/contact.jsx
+++ src/Components/Contact/contact.jsx
@@ -12,20 +12,26 @@ const Contact = () => {
             <ContentBox id="contact">
                     <ContactContent >
                         <ContactTitle >Contact me</ContactTitle>
-                        <form name="contact" id="contactInformation" action="/" method="POST" data-netlify="true">
+                        <form 
+                            name="contact" 
+                            id="contactInformation" 
+                            method="post"
+                            data-netlify="true"
+                            onSubmit="submit"
+                        >
                             
-                            <input type="hidden" name="contact" value="contact" />
+                            <input type="hidden" name="form-name" value="contact" />
                             
                             <ContactLabel htmlFor="name" gridColumn={"1/2"} >Name</ContactLabel>
-                            <ContactTextBox name="name" gridColumn={"1/2"}  />
+                            <ContactTextBox id="name" name="name" gridColumn={"1/2"}  />
                             
                             <ContactLabel htmlFor="email" gridColumn={"1/2"} >Email</ContactLabel>
-                            <ContactTextBox name="email" gridColumn={"1/2"} />
+                            <ContactTextBox id="email" name="email" gridColumn={"1/2"} />
 
                             <ContactLabel htmlFor="message" gridColumn={"2/3"} gridRow={"1/2"} gridC={"1/2"} gridR={"auto"} >Message</ContactLabel>
-                            <ContactTextArea name="message" id="contactArea"/>
+                            <ContactTextArea id="message" name="message" id="contactArea"/>
 
-                            <ContactButton type="reset" >Send</ContactButton>                        
+                            <ContactButton type="submit" >Send</ContactButton>                        
                         </form>
                     </ContactContent>
                 </ContentBox>
@@ -35,20 +41,26 @@ const Contact = () => {
             <ContentBox id="contact" >
                     <ContactContent >
                         <ContactTitle >Contactame</ContactTitle>
-                        <form name="contact" action="/" id="contactInformation" method="POST" data-netlify="true">
+                        <form 
+                            name="contact" 
+                            id="contactInformation" 
+                            method="post" 
+                            data-netlify="true"
+                            onSubmit="submit"
+                        >
                             
                             <input type="hidden" name="form-name" value="contact" />
 
                             <ContactLabel htmlFor="name" gridColumn={"1/2"} >Nombre</ContactLabel>
-                            <ContactTextBox name="name" type="text" gridColumn={"1/2"}  />
+                            <ContactTextBox id="name" name="name" type="text" gridColumn={"1/2"}  />
 
                             <ContactLabel htmlFor="email" gridColumn={"1/2"} >Email</ContactLabel>
-                            <ContactTextBox name="email" type="email" gridColumn={"1/2"} />
+                            <ContactTextBox id="email" name="email" type="email" gridColumn={"1/2"} />
 
                             <ContactLabel htmlFor="message" gridColumn={"2/3"} gridRow={"1/2"} gridC={"1/2"} gridR={"auto"} >Mensaje</ContactLabel>
-                            <ContactTextArea name="message" id="contactArea"/>
+                            <ContactTextArea id="message" name="message" id="contactArea"/>
 
-                            <ContactButton type="reset">Enviar</ContactButton>                        
+                            <ContactButton type="submit">Enviar</ContactButton>                        
                         </form>
                     </ContactContent>
                 </ContentBox>
